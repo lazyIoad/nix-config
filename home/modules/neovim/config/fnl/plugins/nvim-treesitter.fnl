438d240aaf59a4ee28ae0fd1ce7c7e75101f89f1
lazyload <git@lazyloading.net>
2023-10-27 15:49:11 -0400

    add commenting

--- home/modules/neovim/config/fnl/plugins/nvim-treesitter.fnl
+++ home/modules/neovim/config/fnl/plugins/nvim-treesitter.fnl
@@ -1,7 +1,17 @@
-{1 :nvim-treesitter/nvim-treesitter
- :event [:BufReadPost :BufNewFile]
- :main :nvim-treesitter.configs
- :opts {:ensure_installed :all
-        :auto_install true
-        :highlight {:enable true}
-        :indent {:enable true}}}
+[{1 :nvim-treesitter/nvim-treesitter
+  :event [:BufReadPost :BufNewFile]
+  :main :nvim-treesitter.configs
+  :dependencies [{1 :JoosepAlviste/nvim-ts-context-commentstring
+                  :main :ts_context_commentstring
+                  :opts true}]
+  :opts {:ensure_installed :all
+         :auto_install true
+         :highlight {:enable true}
+         :indent {:enable true}
+         :context_commentstring {:enable true :enable_autocmd false}}}
+ {1 :numToStr/Comment.nvim
+  :event [:BufReadPost :BufNewFile]
+  :main :Comment
+  :opts (fn []
+          {:pre_hook ((. (require :ts_context_commentstring.integrations.comment_nvim)
+                         :create_pre_hook))})}]
--- home/modules/neovim/config/lua/plugins/nvim-treesitter.lua
+++ home/modules/neovim/config/lua/plugins/nvim-treesitter.lua
@@ -1,2 +1,5 @@
 -- [nfnl] Compiled from fnl/plugins/nvim-treesitter.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"nvim-treesitter/nvim-treesitter", event = {"BufReadPost", "BufNewFile"}, main = "nvim-treesitter.configs", opts = {ensure_installed = "all", auto_install = true, highlight = {enable = true}, indent = {enable = true}}}
+local function _1_()
+  return {pre_hook = (require("ts_context_commentstring.integrations.comment_nvim")).create_pre_hook()}
+end
+return {{"nvim-treesitter/nvim-treesitter", event = {"BufReadPost", "BufNewFile"}, main = "nvim-treesitter.configs", dependencies = {{"JoosepAlviste/nvim-ts-context-commentstring", main = "ts_context_commentstring", opts = true}}, opts = {ensure_installed = "all", auto_install = true, highlight = {enable = true}, indent = {enable = true}, context_commentstring = {enable = true, enable_autocmd = false}}}, {"numToStr/Comment.nvim", event = {"BufReadPost", "BufNewFile"}, main = "Comment", opts = _1_}}
