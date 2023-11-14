3fca0c175cff4acf93f2fcde39db8d4632e9f4f8
lazyload <git@lazyloading.net>
2023-11-14 17:52:34 -0500

    refactor configuration.nix to be darwin-compatible

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -1,9 +1,14 @@
-{ pkgs, vars, inputs, ... }: {
+{ pkgs, vars, inputs, ... }:
+let
+  inherit (pkgs) stdenv;
+  inherit (pkgs) mkIf;
+in {
   nix = {
     settings.auto-optimise-store = true;
     gc = {
       automatic = true;
-      dates = "weekly";
+      dates = mkIf stdenv.isLinux "weekly";
+      interval.Day = mkIf stdenv.isDarwin 7;
       options = "--delete-older-than 2d";
     };
     package = pkgs.nixFlakes;
@@ -16,51 +21,16 @@
   };
 
   users.users.${vars.user} = {
-    isNormalUser = true;
     description = "${vars.user}";
-    extraGroups = [ "networkmanager" "wheel" "video" ];
     shell = pkgs.fish;
+  } ++ mkIf stdenv.isLinux {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "video" ];
   };
 
   programs.fish.enable = true;
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
-
   time.timeZone = "America/New_York";
 
   nixpkgs.config.allowUnfree = true;
-
-  system.stateVersion = "23.11";
-
-  console.colors = [
-    "1d2021"
-    "3c3836"
-    "504945"
-    "665c54"
-    "bdae93"
-    "d5c4a1"
-    "ebdbb2"
-    "fbf1c7"
-    "fb4934"
-    "fe8019"
-    "fabd2f"
-    "b8bb26"
-    "8ec07c"
-    "83a598"
-    "d3869b"
-    "d65d0e"
-  ];
 }
--- hosts/gaius/default.nix
+++ hosts/gaius/default.nix
@@ -16,10 +16,46 @@
     efi.canTouchEfiVariables = true;
   };
 
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+  system.stateVersion = "23.11";
+
   networking = {
     hostName = "gaius";
     networkmanager.enable = true;
   };
+
+  console.colors = [
+    "1d2021"
+    "3c3836"
+    "504945"
+    "665c54"
+    "bdae93"
+    "d5c4a1"
+    "ebdbb2"
+    "fbf1c7"
+    "fb4934"
+    "fe8019"
+    "fabd2f"
+    "b8bb26"
+    "8ec07c"
+    "83a598"
+    "d3869b"
+    "d65d0e"
+  ];
 
   services.printing.enable = true;
   services.udisks2.enable = true;
