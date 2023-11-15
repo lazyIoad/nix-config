f8c0f8296dc8067ca8684c6b707142a166d5bf04
lazyload <git@lazyloading.net>
2023-11-14 20:44:04 -0500

    add home

--- hosts/darwin-configuration.nix
+++ hosts/darwin-configuration.nix
@@ -1,3 +1,6 @@
-{ pkgs, lib, vars, inputs, ... }: {
+{ vars, ... }: {
   nix.gc.interval.Day = 7;
+  users.users.${vars.user} = {
+    home = "/Users/${vars.user}";
+  };
 }
--- hosts/linux-configuration.nix
+++ hosts/linux-configuration.nix
@@ -1,8 +1,9 @@
-{ pkgs, lib, vars, inputs, ... }: {
+{ vars, ... }: {
   nix.gc.dates = "weekly";
 
   users.users.${vars.user} = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" "video" ];
+    home = "/home/${vars.user}";
   };
 }
