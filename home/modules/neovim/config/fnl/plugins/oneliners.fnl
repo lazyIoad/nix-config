f39124eacfba72b1a9803fefcfa1833758c2eda0
lazyload <git@lazyloading.net>
2023-11-26 22:05:08 -0500

    Add trouble

--- home/modules/neovim/config/fnl/plugins/oneliners.fnl
+++ home/modules/neovim/config/fnl/plugins/oneliners.fnl
@@ -1,3 +1,9 @@
 [{1 :nvim-tree/nvim-web-devicons :lazy true}
  {1 :nvim-lua/plenary.nvim :lazy true}
+ {1 :folke/trouble.nvim
+  :event :VeryLazy
+  :keys [{1 :<leader>t
+          2 (fn []
+              ((. (require :trouble) :toggle) :document_diagnostics))
+          :desc "Open diagnostics"}]}
  {1 :kylechui/nvim-surround :config true :version "*" :event :VeryLazy}]
--- home/modules/neovim/config/lua/plugins/oneliners.lua
+++ home/modules/neovim/config/lua/plugins/oneliners.lua
@@ -1,2 +1,5 @@
 -- [nfnl] Compiled from fnl/plugins/oneliners.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"nvim-tree/nvim-web-devicons", lazy = true}, {"nvim-lua/plenary.nvim", lazy = true}, {"kylechui/nvim-surround", config = true, version = "*", event = "VeryLazy"}}
+local function _1_()
+  return (require("trouble")).toggle("document_diagnostics")
+end
+return {{"nvim-tree/nvim-web-devicons", lazy = true}, {"nvim-lua/plenary.nvim", lazy = true}, {"folke/trouble.nvim", event = "VeryLazy", keys = {{"<leader>t", _1_, desc = "Open diagnostics"}}}, {"kylechui/nvim-surround", config = true, version = "*", event = "VeryLazy"}}
