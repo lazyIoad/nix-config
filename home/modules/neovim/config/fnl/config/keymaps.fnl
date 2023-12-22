93f6f07e6f6f1bbcbab2a3673b0eef0baaf661f4
lazyload <git@lazyloading.net>
2023-12-22 13:49:00 -0500

    Make neovim keymaps silent

--- home/modules/neovim/config/fnl/config/keymaps.fnl
+++ home/modules/neovim/config/fnl/config/keymaps.fnl
@@ -1,5 +1,5 @@
 (macro keybind [mode lhs rhs desc]
-  `(vim.keymap.set ,mode ,lhs ,rhs {:desc ,desc}))
+  `(vim.keymap.set ,mode ,lhs ,rhs {:desc ,desc :silent true}))
 
 (keybind :n :<A-l> ":bnext<CR>" "Next buffer")
 (keybind :n :<A-h> ":bprevious<CR>" "Previous buffer")
--- home/modules/neovim/config/lua/config/keymaps.lua
+++ home/modules/neovim/config/lua/config/keymaps.lua
@@ -1,3 +1,3 @@
 -- [nfnl] Compiled from fnl/config/keymaps.fnl by https://github.com/Olical/nfnl, do not edit.
-vim.keymap.set("n", "<A-l>", ":bnext<CR>", {desc = "Next buffer"})
-return vim.keymap.set("n", "<A-h>", ":bprevious<CR>", {desc = "Previous buffer"})
+vim.keymap.set("n", "<A-l>", ":bnext<CR>", {desc = "Next buffer", silent = true})
+return vim.keymap.set("n", "<A-h>", ":bprevious<CR>", {desc = "Previous buffer", silent = true})
