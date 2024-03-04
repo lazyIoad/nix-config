9964990ef0032f7847ef0b5b11d8f3fafb675078
lazyload <git@lazyloading.net>
2024-03-04 14:46:16 -0500

    Fix fzf grep

--- res/configs/nvim/lua/plugins/fzf-lua.lua
+++ res/configs/nvim/lua/plugins/fzf-lua.lua
@@ -1,20 +1,26 @@
 return {
-  "ibhagwan/fzf-lua",
-  keys = {
-    {
-      "<leader>ff",
-      function() require("fzf-lua").files() end,
-      desc = "Find file"
-    },
-    {
-      "<leader>fb",
-      function() require("fzf-lua").buffers() end,
-      desc = "Find buffer"
-    },
-    {
-      "<leader>fg",
-      function() require("fzf-lua").grep() end,
-      desc = "Search in files"
-    }
-  }
+	"ibhagwan/fzf-lua",
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "Find file",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Find buffer",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("fzf-lua").grep_visual()
+			end,
+			desc = "Search in files",
+		},
+	},
 }
